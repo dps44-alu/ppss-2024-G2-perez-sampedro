d8a315a3cb3fdc25edd75e569c942b37fe058587
David Perez Sampedro <dps44@alu.ua.es>
2024-02-13 08:59:31 +0100

    Cambios en CineTest y añadadio ButacasException

--- P02-Drivers/drivers/src/test/java/ppss/CineTest.java
+++ P02-Drivers/drivers/src/test/java/ppss/CineTest.java
@@ -2,7 +2,11 @@ package ppss;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.rmi.UnexpectedException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CineTest {
     Cine cine;
@@ -14,10 +18,14 @@ public class CineTest {
 
     @Test
     public void C1_reservaButacas_should_return_Exception_when_fila_empty_and_want_3() {
-        asientos = [];
-        solicitados = 3;
-        resultadoEsperado = "No se puede procesar la solicitud";
-        resultadoReal = cine.reservarButacas(asientos, solicitados);
-        assertEquals(resultadoEsperado, resultadoReal);
+        boolean[] asientos = new boolean[0];
+        int solicitados = 3;
+
+        String resultadoEsperado = "No se puede procesar la solicitud";
+        Boolean resultadoReal = cine.reservaButacas(asientos, solicitados);
+
+        UnexpectedException exception = assertThrows(ButacasException.class, () -> cine.reservaButacas(asientos, solicitados));
+
+        assertEquals(resultadoEsperado, exception.getMessage());
     }
 }
