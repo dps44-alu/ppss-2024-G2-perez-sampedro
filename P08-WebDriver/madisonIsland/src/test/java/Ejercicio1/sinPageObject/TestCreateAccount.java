1566bdf30977fdd91e27a75baa7773dedfb857af
dps44-alu <dps44@alu.ua.es>
2024-05-05 21:56:57 +0200

    P08-E1 plantilla

---
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
@@ -0,0 +1,12 @@
+package Ejercicio1.sinPageObject;
+
+import org.junit.jupiter.api.*;
+
+public class TestCreateAccount {
+
+    @Test
+    @Tag("OnlyOnce")
+    void S1_scenario_createAccount_should_create_new_account_in_the_demo_store_when_this_account_does_not_exist() {
+
+    }
+}
---
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
@@ -0,0 +1,16 @@
+package Ejercicio1.sinPageObject;
+
+import org.junit.jupiter.api.*;
+
+public class TestLogin {
+
+    @Test
+    void S2_scenario_loginOK_should_login_with_success_when_user_account_exists() {
+
+    }
+
+    @Test
+    void S3_scenario_loginFailed_should_fail_when_user_account_not_exists() {
+
+    }
+}
